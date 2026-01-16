764342771e7bee71961ecce6de1631990af762c1
fortes42 <forteslv42@gmail.com>
2026-01-17 03:05:39 +0900

    feat: 커뮤니티 메인 페이지 충돌 해결 (#29)
    - 충돌 해결

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,21 @@
+import { Toaster } from '@/shared/ui/Toaster'
+import Footer from '@/shared/ui/Footer'
+import { Modal, ModalClose } from '@/shared/ui/Modal'
+import Header from '@/shared/ui/header/Header'
+import { Button, ButtonVariants } from '@/shared/ui/Button'
+import { Input } from '@/shared/ui/input/Input'
+import { Pagination } from '@/shared/ui/Pagination'
+import { DropdownMenu } from '@/shared/ui/DropdownMenu'
+
+export {
+  Toaster,
+  Footer,
+  Modal,
+  ModalClose,
+  Header,
+  Button,
+  ButtonVariants,
+  Input,
+  Pagination,
+  DropdownMenu,
+}
