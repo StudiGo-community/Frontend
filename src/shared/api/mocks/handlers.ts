4e7146267d5d1f7bb4efae58a3bd5d13063702f5
JihyeKim <jihyekim830@gmail.com>
2026-01-19 17:17:07 +0900

    chore: MSW 예시용 핸들러 URL 수정 (#38)

--- src/shared/api/mocks/handlers.ts
+++ src/shared/api/mocks/handlers.ts
@@ -1,7 +1,7 @@
 import { http, HttpResponse } from 'msw'
 
 const handlers = [
-  http.get('/health-check', () => {
+  http.get('http://localhost:3000/health-check', () => {
     return HttpResponse.json({ message: 'Hello!' })
   }),
 ]
