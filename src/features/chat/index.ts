83a544cd17a01d0b1978b5b0a875e660f931dfce
JihyeKim <jihyekim830@gmail.com>
2026-01-24 04:11:51 +0900

    feat: 채팅방 상세 페이지 생성 (#52)

--- src/features/chat/index.ts
+++ src/features/chat/index.ts
@@ -1,4 +1,15 @@
 import ChatRoomFilter from '@/features/chat/ui/ChatRoomFilter'
 import ChatRoomList from '@/features/chat/ui/ChatRoomList'
+import ChatHeader from '@/features/chat/ui/ChatHeader'
+import ChatSidebar from '@/features/chat/ui/ChatSidebar'
+import MessageInput from '@/features/chat/ui/MessageInput'
+import MessageList from '@/features/chat/ui/MessageList'
 
-export { ChatRoomFilter, ChatRoomList }
+export {
+  ChatRoomFilter,
+  ChatRoomList,
+  ChatHeader,
+  ChatSidebar,
+  MessageInput,
+  MessageList,
+}
