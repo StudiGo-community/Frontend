36f29ddcf7ec51d4565dbc71c6fd815b52fa6da7
JihyeKim <jihyekim830@gmail.com>
2026-02-02 07:08:36 +0900

    feat: 받은 메세지 컴포넌트에 TTS 기능 적용 (#90)

--- src/features/chat-message-subscribe/model/useMessageSubscribe.ts
+++ src/features/chat-message-subscribe/model/useMessageSubscribe.ts
@@ -3,7 +3,10 @@ import { ChatSocketEventSchema } from '@/features/chat-message-subscribe/model/s
 import useMessageCacheHandler from '@/entities/message/model/useMessageCacheHandler'
 import { useChatSocketStore } from '@/features/chat-message-subscribe/model/store'
 
-function useMessageSubscribe(roomId: number | null, accessToken: string) {
+function useMessageSubscribe(
+  roomId: number | null,
+  accessToken: string | null
+) {
   const { connect } = useChatSocketStore()
   const { handleNewMessage } = useMessageCacheHandler()
 
