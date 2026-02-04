a110dd92c0f297dc26b810346843fb93ff85608e
JihyeKim <jihyekim830@gmail.com>
2026-02-04 11:58:45 +0900

    feat: 채팅방 입장 API 테스트 및 데이터 접근 로직 수정 (#105)

--- src/shared/api/mocks/handlers/chat-handlers.ts
+++ src/shared/api/mocks/handlers/chat-handlers.ts
@@ -37,36 +37,36 @@ import {
 // )
 
 // ---------- 채팅방 입장 ----------
-const enterChatRoom = http.post<{ roomId?: string }>(
-  `${process.env.NEXT_PUBLIC_API_BASE_URL}/chat/:roomId`,
-  async ({ params }) => {
-    const { roomId } = params
-    const parsedRoomId = Number(roomId)
+// const enterChatRoom = http.post<{ roomId?: string }>(
+//   `${process.env.NEXT_PUBLIC_API_BASE_URL}/chat/:roomId`,
+//   async ({ params }) => {
+//     const { roomId } = params
+//     const parsedRoomId = Number(roomId)
 
-    return await new Promise((resolve) => setTimeout(resolve, 3000)).then(() =>
-      HttpResponse.json({
-        message: '채팅방에 입장했습니다.',
-        room: {
-          id: parsedRoomId,
-          name: CHAT_ROOMS.find((room) => room.id === parsedRoomId)?.name,
-        },
-      })
-    )
-    // if ([1, 2, 3, 4].includes(parsedRoomId ?? '')) {
-    //   return HttpResponse.json({
-    //     message: '채팅방에 입장했습니다.',
-    //     room: {
-    //       id: parsedRoomId,
-    //       name: CHAT_ROOMS.find((room) => room.id === parsedRoomId)?.name,
-    //     },
-    //   })
-    // }
-    // return HttpResponse.json(
-    //   { detail: '채팅방을 찾을 수 없습니다.' },
-    //   { status: 404 }
-    // )
-  }
-)
+//     return await new Promise((resolve) => setTimeout(resolve, 3000)).then(() =>
+//       HttpResponse.json({
+//         message: '채팅방에 입장했습니다.',
+//         room: {
+//           id: parsedRoomId,
+//           name: CHAT_ROOMS.find((room) => room.id === parsedRoomId)?.name,
+//         },
+//       })
+//     )
+//     // if ([1, 2, 3, 4].includes(parsedRoomId ?? '')) {
+//     //   return HttpResponse.json({
+//     //     message: '채팅방에 입장했습니다.',
+//     //     room: {
+//     //       id: parsedRoomId,
+//     //       name: CHAT_ROOMS.find((room) => room.id === parsedRoomId)?.name,
+//     //     },
+//     //   })
+//     // }
+//     // return HttpResponse.json(
+//     //   { detail: '채팅방을 찾을 수 없습니다.' },
+//     //   { status: 404 }
+//     // )
+//   }
+// )
 
 // ---------- 채팅 메세지 조회 ----------
 const getChatMessageList = http.get(
@@ -186,7 +186,7 @@ const chatSocketHandlers = [
 
 const chatHandlers = [
   // getChatRoomList,
-  enterChatRoom,
+  // enterChatRoom,
   getChatMessageList,
   sendChatMessage,
   exitChatRoom,
