ee22935792d91863bc313e8915186fe0c8a2762c
Nagi-ei <nakt4453@gmail.com>
2026-02-06 05:19:06 +0900

    feat: 컨펌 모달 공통 컴포넌트 제작 및 테스트 (#116)

--- src/features/community-post-like/ui/PostLikeButton.tsx
+++ src/features/community-post-like/ui/PostLikeButton.tsx
@@ -1,8 +1,10 @@
 'use client'
 
+import { useState } from 'react'
 import { Heart } from 'lucide-react'
 import { Button } from '@/shared/ui/Button'
 import { cn } from '@/shared/lib/cn'
+import { ConfirmModal } from '@/shared/ui/ConfirmModal'
 
 interface PostLikeButtonProps {
   isLiked: boolean
@@ -15,22 +17,42 @@ export default function PostLikeButton({
   onClick,
   className,
 }: PostLikeButtonProps) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleConfirm = () => {
+    console.log('좋아요')
+    setIsModalOpen(false)
+    onClick?.()
+  }
+
   return (
-    <Button
-      variant="outline"
-      size="sm"
-      onClick={onClick}
-      className={cn(
-        'hover:bg-brand-gray-100 w-24 text-sm transition-all duration-200',
-        className
-      )}
-    >
-      <Heart
-        size={14}
-        strokeWidth={2}
-        className={cn('text-brand-third', isLiked && 'fill-brand-third')}
-      />
-      <span>좋아요</span>
-    </Button>
+    <>
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => setIsModalOpen(true)}
+        className={cn(
+          'hover:bg-brand-gray-100 w-24 text-sm transition-all duration-200',
+          className
+        )}
+      >
+        <Heart
+          size={14}
+          strokeWidth={2}
+          className={cn('text-brand-third', isLiked && 'fill-brand-third')}
+        />
+        <span>좋아요</span>
+      </Button>
+
+      <ConfirmModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        onConfirm={handleConfirm}
+        title="알림"
+      >
+        게시글을 <strong className="text-brand-second">좋아요</strong>{' '}
+        하시겠습니까?
+      </ConfirmModal>
+    </>
   )
 }
