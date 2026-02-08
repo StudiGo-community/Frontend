f666cf3f1dd7d1e20ad5af7caae430c0c70283d9
JihyeKim <jihyekim830@gmail.com>
2026-02-08 16:58:02 +0900

    feat: 실시간 채팅 시현을 위한 웹소켓 MSW 핸들러 추가 (#143)

--- src/shared/api/mocks/handlers/handlers.ts
+++ src/shared/api/mocks/handlers/handlers.ts
@@ -1,6 +1,6 @@
 import { http, HttpResponse, passthrough } from 'msw'
 import { communityHandlers } from '@/shared/api/mocks/handlers/community-handlers'
-// import { chatHandlers } from '@/shared/api/mocks/handlers/chat-handlers'
+import { chatHandlers } from '@/shared/api/mocks/handlers/chat-handlers'
 import { mypageHandlers } from '@/shared/api/mocks/handlers/mypage-handlers'
 // import { authHandlers } from '@/shared/api/mocks/handlers/auth-handlers'
 
@@ -15,7 +15,7 @@ const handlers = [
   http.post('/api/auth/logout', () => passthrough()),
 
   ...communityHandlers,
-  // ...chatHandlers,
+  ...chatHandlers,
   ...mypageHandlers,
 
   // ...authHandlers,
