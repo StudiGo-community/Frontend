f7c4b4a810324f896e3e68519aaf6a48b104ea5d
Nagi-ei <nakt4453@gmail.com>
2026-02-09 10:04:05 +0900

    feat: 데일리 배너 로딩 에러 처리 (#145)

--- src/widgets/community-quiz/ui/CommunityBanner.tsx
+++ src/widgets/community-quiz/ui/CommunityBanner.tsx
@@ -1,17 +1,9 @@
 import CommunityBannerClient from '@/widgets/community-quiz/ui/CommunityBannerClient'
-import { Suspense } from 'react'
-import ApiErrorBoundary from '@/shared/ui/ApiErrorBoundary'
 import { getQuote } from '@/widgets/community-quiz/api/getQuote'
 import { getQuiz } from '@/widgets/community-quiz/api/getQuiz'
 
 export default async function CommunityBanner() {
   const [quote, quiz] = await Promise.all([getQuote(), getQuiz()])
 
-  return (
-    <ApiErrorBoundary>
-      <Suspense fallback={<div>로딩중...</div>}>
-        <CommunityBannerClient quiz={quiz} quote={quote} />
-      </Suspense>
-    </ApiErrorBoundary>
-  )
+  return <CommunityBannerClient quiz={quiz} quote={quote} />
 }
